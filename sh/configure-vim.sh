d5f58c83c07cd349fb81680d9d31699de74f0ab0
Tom George <tg82490@gmail.com>
2016-02-17 15:56:30 +0000

    Pull repo if my vimfiles exist already

--- sh/configure-vim.sh
+++ sh/configure-vim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/tomgeorge/vimfiles /home/vagrant/.vim || true
+git clone https://github.com/tomgeorge/vimfiles /home/vagrant/.vim || cd /home/vagrant.vim && git pull origin master
 vi +PluginInstall +qall
 chown -R vagrant:vagrant /home/vagrant/.vim
