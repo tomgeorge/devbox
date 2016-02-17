f4611406fbecd898109c5fd0674449abec017d72
Tom George <tg82490@gmail.com>
2016-02-17 15:24:06 +0000

    pull if dotfiles repo exists, install oh-my-zsh from my fork

--- sh/install-oh-my-zsh.sh
+++ sh/install-oh-my-zsh.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/robbyrussell/oh-my-zsh.git /home/vagrant/.oh-my-zsh || true
+git clone https://github.com/tomgeorge/oh-my-zsh.git /home/vagrant/.oh-my-zsh || true
 chown -R vagrant:vagrant /home/vagrant/.oh-my-zsh
 chsh -s /bin/zsh vagrant
--- sh/set-up-dotfiles.sh
+++ sh/set-up-dotfiles.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-git clone https://github.com/tomgeorge/dotfiles /home/vagrant/dotfiles || true
+git clone https://github.com/tomgeorge/dotfiles /home/vagrant/dotfiles || cd /home/vagrant/dotfiles && git pull origin master
 chown -R vagrant:vagrant /home/vagrant/dotfiles
 bash /home/vagrant/dotfiles/links-vagrant.sh
