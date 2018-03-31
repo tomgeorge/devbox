777002d79b6842c466de2835b6c335220cdfa266
Tom George <tg82490@gmail.com>
2018-03-31 17:05:44 +0000

    Remove add user shell, add to provisioning droplet shell

--- make_dev_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER_ID=${LOCAL_USER_ID:-1000}
-USER_NAME=${LOCAL_USER_NAME:-dev}
-
-echo "creating with UID : $USER_ID"
-useradd --shell /bin/bash -u "${USER_ID}" -o -c "" -m dev
-echo "dev:dev" | chpasswd
-usermod -aG sudo dev
